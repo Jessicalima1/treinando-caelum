d73a6ec214824673f194504809322bc03c3124c8
Jessicalima1 <jee_014@hotmail.com>
2021-04-05 21:00:03 -0300

    Treinando caelum

---
+++ src/br/com/jessy/caelum/cap3/Main.java
@@ -0,0 +1,15 @@
+package br.com.jessy.caelum.cap3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int i = 150; i <= 300; i++){
+			System.out.println(i);
+		}
+
+	
+
+	}
+
+}
